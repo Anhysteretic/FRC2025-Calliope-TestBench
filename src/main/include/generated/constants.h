e82376567549505f47220f50ff3480c138f97a3c
David Tian <dtian.github@outlook.com>
2025-01-22 18:37:01 -0800

    pathfinder working

---
+++ src/main/include/generated/constants.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "frc/geometry/Pose2d.h"
+#include "frc/geometry/Rotation2d.h"
+#include "frc/geometry/Translation2d.h"
+#include "pathplanner/lib/config/ModuleConfig.h"
+#include "units/moment_of_inertia.h"
+#include <pathplanner/lib/config/RobotConfig.h>
+#include <frc/system/plant/DCMotor.h>
+#include <vector>
+
+class pathplannerConstants{
+    public:
+        inline static const pathplanner::RobotConfig config = pathplanner::RobotConfig(
+            49.8952_kg,
+            units::kilogram_square_meter_t{4.6468320808371},
+            pathplanner::ModuleConfig(
+                2_in,
+                5_mps,
+                1.5,
+                frc::DCMotor::KrakenX60(),
+                5.143,
+                60_A,
+                1
+            ),
+            std::vector<frc::Translation2d>(
+                {
+                    frc::Translation2d(10.3925_in, 10.3925_in),
+                    frc::Translation2d(10.3925_in, -10.3925_in),
+                    frc::Translation2d(-10.3925_in, 10.3925_in),
+                    frc::Translation2d(-10.3925_in, -10.3925_in)
+                }
+            )
+        );
+        
+        inline static const frc::Pose2d target = frc::Pose2d(536.961_in, 100.424_in, frc::Rotation2d(210_deg));
+
+        //inline static const frc::Pose2d target = frc::Pose2d(536.961_in, 119.424_in, frc::Rotation2d(210_deg));
+};
